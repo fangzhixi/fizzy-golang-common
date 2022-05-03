806826667f768b8321937a546094618ebfab9287
方志熹 <zhixi.fang@fowo.com>
2022-05-03 16:16:31 +0800

    修正错误码定义

--- error/errcode/fizzy_ocr.go
+++ error/errcode/fizzy_ocr.go
@@ -3,13 +3,13 @@ package errcode
 import "fmt"
 
 const (
-	FIZZY_OCR_PARAM_VERIFY_FAILED_        = 1100 //业务参数校验不通过
+	FIZZY_OCR_PARAM_VERIFY_FAILED        = 1100 //业务参数校验不通过
 	FIZZY_OCR_IMAGE_URL_PARAM_NOT_NULL    = 1101 //image_url不能为空
 	FIZZY_OCR_IMAGE_BASE64_PARAM_NOT_NULL = 1102 //image_base64不能为空
 )
 
 var FizzyOcrErrorMsg = map[int32]string{
-	FIZZY_OCR_PARAM_VERIFY_FAILED_:        "业务参数校验不通过",
+	FIZZY_OCR_PARAM_VERIFY_FAILED:        "业务参数校验不通过",
 	FIZZY_OCR_IMAGE_URL_PARAM_NOT_NULL:    "image_url不能为空",
 	FIZZY_OCR_IMAGE_BASE64_PARAM_NOT_NULL: "image_base64不能为空",
 }
